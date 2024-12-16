6a10b89db0f80c94e5f59703786fc0640fcdfaff
Elisabetta maragò <elisabettamarago@Mac-mini-di-Elisabetta.local>
2024-12-16 17:34:42 +0100

    Caricamento iniziale del progetto

---
+++ index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"))
+
+app.use(bodyParser.urlencoded({extended:true}))
+let posts =[];
+
+app.get("/", (req, res) => {
+    res.render("home.ejs");
+  });
+  
+  app.get("/new-post", (req, res) => {
+    res.render("new-post.ejs");
+  });
+
+  app.post("/new-post", (req, res) => {
+    const post = {
+      title: req.body.title,
+      content: req.body.content,
+    };
+    posts.push(post);
+    res.redirect("/");
+  });
+  app.get("/post/:id", (req, res) => {
+    const post = posts[req.params.id];
+    if (post) {
+      res.render("post", { post: post });
+    } else {
+      res.status(404).send("Post not found");
+    }
+  });
+app.listen(port,() =>{
+    console.log(`Listening on port ${port}`);
+})
